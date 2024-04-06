cb6615a463f44d2519a5d9ee0192ef3d95745aa0
Adam Taylor <adam.taylor@sagebase.org>
2024-04-06 14:28:48 +0100

    Refactor

--- reset_dev.py
+++ reset_dev.py
@@ -1,4 +1,5 @@
 import synapseclient
+import json
 
 syn = synapseclient.Synapse()
 syn.login()
@@ -6,6 +7,37 @@ syn.login()
 # Your project ID and the FileView ID
 project_id = 'syn55259805'
 fileview_id = 'syn55259830'
+
+
+def verify_acl_inheritance(syn, project_id, fileview_id):
+    """
+    Verifies that all entities within a specified project inherit their ACLs directly from the project.
+
+    This function queries a FileView to find entities whose benefactorId does not match the project ID, indicating they do not inherit their ACLs directly from the project. It prints a warning with the IDs of such entities if any are found. If no entities are found that deviate from the project's ACL, it confirms successful inheritance.
+
+    Global Variables Used:
+    - syn (synapseclient.Synapse): A logged-in Synapse client instance.
+    - fileview_id (str): The Synapse ID of the FileView used for the query.
+    - project_id (str): The Synapse ID of the project against which entities' ACL inheritance is verified.
+
+    Exceptions:
+    - May raise exceptions related to querying the FileView or processing the query results, propagated from the `synapseclient` methods used.
+
+    Side Effects:
+    - Makes a call to the Synapse REST API to perform a query on a FileView.
+    - Prints to the console the outcome of the verification.
+    """
+    query_str = f"SELECT id FROM {fileview_id} WHERE benefactorId<>'{project_id}' AND projectId='{project_id}'"
+    query_results = syn.tableQuery(query_str)
+    entities_df = query_results.asDataFrame()
+
+    if not entities_df.empty:
+        print(f"Warning: There are {len(entities_df)} entities that do not inherit permissions from the project.")
+        print(entities_df['id'])
+        return(entities_df)
+    else:
+        print("Success: All entities inherit permissions from the project.")
+        exit()
 
 def delete_acl(entity_id):
     """
@@ -33,42 +65,13 @@ def delete_acl(entity_id):
         else:
             raise
 
-def verify_acl_inheritance():
-    """
-    Verifies that all entities within a specified project inherit their ACLs directly from the project.
+def get_acl(entity_id):
+    uri = f"/entity/{entity_id}/acl"
+    acl = syn.restGET(uri)
+    return(acl) 
 
-    This function queries a FileView to find entities whose benefactorId does not match the project ID, indicating they do not inherit their ACLs directly from the project. It prints a warning with the IDs of such entities if any are found. If no entities are found that deviate from the project's ACL, it confirms successful inheritance.
 
-    Global Variables Used:
-    - syn (synapseclient.Synapse): A logged-in Synapse client instance.
-    - fileview_id (str): The Synapse ID of the FileView used for the query.
-    - project_id (str): The Synapse ID of the project against which entities' ACL inheritance is verified.
-
-    Exceptions:
-    - May raise exceptions related to querying the FileView or processing the query results, propagated from the `synapseclient` methods used.
-
-    Side Effects:
-    - Makes a call to the Synapse REST API to perform a query on a FileView.
-    - Prints to the console the outcome of the verification.
-    """
-    query_str = f"SELECT id FROM {fileview_id} WHERE benefactorId<>'{project_id}'"
-    query_results = syn.tableQuery(query_str)
-    entities_df = query_results.asDataFrame()
-
-    if not entities_df.empty:
-        print(f"Warning: There are {len(entities_df)} entities that do not inherit permissions from the project.")
-        print(entities_df['id'])
-    else:
-        print("Success: All entities inherit permissions from the project.")
-
-# Query the FileView for unique benefactor IDs
-query_str = f"SELECT DISTINCT(benefactorId) as benefactorId FROM {fileview_id} WHERE projectId='{project_id}' AND benefactorId <> '{project_id}'"
-query_results = syn.tableQuery(query_str)
-
-# Fetch the results into a DataFrame
-benefactors_df = query_results.asDataFrame()
-
-print(benefactors_df)
+benefactors_df = verify_acl_inheritance(syn, project_id, fileview_id)
 
 response = input("Press 'y' to continue or 'n' to exit: ").lower()
 if response == 'y':
@@ -80,10 +83,17 @@ else:
     print("Invalid input. Exiting script.")
     exit()
 
+acls = []
 
-for b in benefactors_df['benefactorId']:
+for b in benefactors_df['id']:
+    acl = get_acl(b)
+    acls.append(acl)
     delete_acl(b)
 
-verify_acl_inheritance()
+with open('deleted_acls.json', 'w', encoding='utf-8') as f:
+    json.dump(acls, f, indent=2)
+
+# Confirm that all ACLs have been deleted
+verify_acl_inheritance(syn, project_id, fileview_id)
 
 
