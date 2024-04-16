4189ba57d72a6cb4f9feb5147354ec1f18071046
Clarisse Lau <63608514+clarisse-lau@users.noreply.github.com>
2024-04-16 13:41:56 -0700

    Modify public entity query

--- make_L3_L3_public.py
+++ make_L3_L3_public.py
@@ -51,7 +51,7 @@ query = """
         )
         SELECT entityId AS syn_public FROM released
         WHERE REGEXP_CONTAINS(Component,
-        r'%Level[34]|Auxiliary|Accessory|ExSeqMinimal|Slide-seq|MassSpectrometry|RPPA')
+        r'%Level[34]|Auxiliary|Accessory|Other|MassSpectrometry|RPPA')
         UNION ALL
         SELECT DISTINCT channel_metadata_synapseId AS syn_public FROM released
         WHERE channel_metadata_synapseId IS NOT NULL
